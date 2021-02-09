a51571e4a55034f83abe2910eddeae0501f01f00
Ismael Hernandez Munoz <ismael.hernandezm@outlook.com>
2021-02-09 11:03:00 +0100

    añadido opciones al menu y toast al clickarlas

--- Fundamentals04.2-Input-controls/app/src/main/java/com/example/clickableimages/MainActivity.java
+++ Fundamentals04.2-Input-controls/app/src/main/java/com/example/clickableimages/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -57,5 +59,20 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_action1:
+                displayToast("action 1");
+                return true;
+            case R.id.action_action2:
+                displayToast("action 2");
+                return true;
+            default:
+                return false;
+        }
+
     }
 }
